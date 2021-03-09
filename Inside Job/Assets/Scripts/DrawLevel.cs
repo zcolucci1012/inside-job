5f53e791a99ba25c177f76af0ee50b2bb6d8ef5f
zcolucci1012 <32983507+zcolucci1012@users.noreply.github.com>
2021-03-08 23:16:15 -0500

    Room Generation + Player Model
    - Added functionality for random room locations
    - Implemented player model
    - Player moves and collides with walls
    - Camera follows player

--- Inside Job/Assets/Scripts/DrawLevel.cs
+++ Inside Job/Assets/Scripts/DrawLevel.cs
@@ -1,13 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class DrawLevel : MonoBehaviour
 {
+    private Tilemap[] floors;
+    private Tilemap[] walls;
+    private Tile[] tiles;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        tiles = Resources.LoadAll<Tile>("Tiles/Floor1Tiles");
+
+        int numRooms = Random.Range(8, 12);
+        GenerateMap(numRooms);
+
+    }
+
+    void GenerateMap(int numRooms)
+    {
+        int[,] rooms = new int[numRooms, 2];
+        int[,] doors = new int[numRooms - 1, 2];
+
+        rooms[0, 0] = 0;
+        rooms[0, 1] = 0;
+
+        for (int ii = 1; ii < numRooms; ii++)
+        {
+            
+            int ss = Random.Range(0, ii);
+            
+            int dx;
+            int dy;
+            int direction = Random.Range(0, 4);
+            switch (direction)
+            {
+                case 0:
+                    dx = 0;
+                    dy = 1;
+                    break;
+                case 1:
+                    dx = 1;
+                    dy = 0;
+                    break;
+                case 2:
+                    dx = 0;
+                    dy = -1;
+                    break;
+                default:
+                    dx = -1;
+                    dy = 0;
+                    break;
+            }
+
+            int[] newRoom = new int[2] { rooms[ss,0] + dx, rooms[ss,1] + dy};
+
+            bool found = false;
+            for (int jj = 0; jj < ii; jj++)
+            {
+                if (rooms[jj, 0] == newRoom[0] && rooms[jj, 1] == newRoom[1])
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                ii--;
+            } else
+            {
+                rooms[ii, 0] = newRoom[0];
+                rooms[ii, 1] = newRoom[1];
+                doors[ii - 1, 0] = ss;
+                doors[ii - 1, 1] = ii;
+            }
+        }
     }
 
     // Update is called once per frame
---
+++ Inside Job/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform cameraPosition;
+
+    private float runSpeed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+
+        float xVel = 0;
+        float yVel = 0;
+
+        if (Input.GetKey("up") || Input.GetKey("w"))
+        {
+            yVel += runSpeed;
+        }
+        if (Input.GetKey("left") || Input.GetKey("a"))
+        {
+            xVel -= runSpeed;
+        }
+        if (Input.GetKey("down") || Input.GetKey("s"))
+        {
+            yVel -= runSpeed;
+        }
+        if (Input.GetKey("right") || Input.GetKey("d"))
+        {
+            xVel += runSpeed;
+        }
+
+        rigidbody.velocity = new Vector2(xVel, yVel);
+
+        cameraPosition.position = new Vector3(this.transform.position.x,
+            this.transform.position.y,
+            cameraPosition.position.z);
+    }
+}
